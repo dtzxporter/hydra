5ebd3cb84b0aaaa3d613e2d6a180124276ff2f62
dtzxporter <dtzxporter@users.noreply.github.com>
2024-05-01 22:26:39 -0400

    More supervisor work.

--- hydra/src/supervisor.rs
+++ hydra/src/supervisor.rs
@@ -1,14 +1,23 @@
+use std::collections::BTreeSet;
+use std::collections::VecDeque;
 use std::pin::Pin;
 use std::time::Duration;
+use std::time::Instant;
 
 use crate::AutoShutdown;
 use crate::ChildSpec;
+use crate::ChildType;
 use crate::ExitReason;
 use crate::GenServer;
 use crate::GenServerOptions;
+use crate::Message;
 use crate::Pid;
 use crate::Process;
 use crate::ProcessFlags;
+use crate::Reference;
+use crate::Restart;
+use crate::Shutdown;
+use crate::SystemMessage;
 
 /// A supervision child.
 struct SupervisedChild {
@@ -32,6 +41,7 @@ pub enum SupervisionStrategy {
 /// Supervision trees provide fault-tolerance and encapsulate how our applications start and shutdown.
 pub struct Supervisor {
     children: Vec<SupervisedChild>,
+    restarts: VecDeque<Instant>,
     strategy: SupervisionStrategy,
     auto_shutdown: AutoShutdown,
     max_restarts: usize,
@@ -43,6 +53,7 @@ impl Supervisor {
     pub const fn new() -> Self {
         Self {
             children: Vec::new(),
+            restarts: VecDeque::new(),
             strategy: SupervisionStrategy::OneForOne,
             auto_shutdown: AutoShutdown::Never,
             max_restarts: 3,
@@ -103,38 +114,66 @@ impl Supervisor {
         GenServer::start_link(self, (), options).await
     }
 
+    /// Starts all of the children.
     async fn start_children(&mut self) -> Result<(), ExitReason> {
-        for child in self.children.iter_mut() {
+        let mut remove: Vec<usize> = Vec::new();
+
+        for (index, child) in self.children.iter_mut().enumerate() {
             let start_child = Pin::from(child.spec.start.as_ref().unwrap()()).await;
 
             match start_child {
-                Ok(pid) => child.pid = Some(pid),
+                Ok(pid) => {
+                    child.pid = Some(pid);
+                }
                 Err(reason) => {
                     if reason.is_ignore() {
+                        if child.is_temporary() {
+                            remove.push(index);
+                        }
                         continue;
                     }
 
-                    return Err(reason);
+                    return Err(ExitReason::from("failed_to_start_child"));
                 }
             }
         }
 
-        Ok(())
-    }
-
-    async fn terminate_children(&mut self) -> Result<(), ExitReason> {
-        for child in self.children.iter_mut().rev() {
-            let Some(pid) = child.pid.take() else {
-                continue;
-            };
-
-            Process::exit(pid, ExitReason::from("shutdown"));
+        for index in remove.into_iter().rev() {
+            self.children.remove(index);
         }
 
         Ok(())
     }
 
+    /// Terminates all of the children.
+    async fn terminate_children(&mut self) -> Result<(), ExitReason> {
+        let mut remove: Vec<usize> = Vec::new();
+
+        for (index, child) in self.children.iter_mut().enumerate().rev() {
+            if child.is_temporary() {
+                remove.push(index);
+            }
+
+            let Some(pid) = child.pid.take() else {
+                continue;
+            };
+
+            if let Err(reason) = shutdown(pid, child.shutdown()).await {
+                println!("Report error: {:?}", reason);
+            }
+        }
+
+        for index in remove {
+            self.children.remove(index);
+        }
+
+        Ok(())
+    }
+
+    /// Checks all of the children for correct specification and then starts them.
     async fn init_children(&mut self) -> Result<(), ExitReason> {
+        self.check_child_specs();
+
         if let Err(reason) = self.start_children().await {
             let _ = self.terminate_children().await;
 
@@ -142,6 +181,58 @@ impl Supervisor {
         }
 
         Ok(())
+    }
+
+    async fn restart_child(&mut self, pid: Pid, reason: ExitReason) -> Result<(), ExitReason> {
+        let Some(index) = self.find_child(pid) else {
+            return Ok(());
+        };
+
+        // https://github.com/erlang/otp/blob/master/lib/stdlib/src/supervisor.erl#L1157
+        let child = &mut self.children[index];
+
+        // This child has died, do something!
+
+        Ok(())
+    }
+
+    fn add_restart(&mut self) {
+        // https://github.com/erlang/otp/blob/master/lib/stdlib/src/supervisor.erl#L2075
+        let now = Instant::now();
+    }
+
+    fn check_child_specs(&mut self) {
+        let mut ids: BTreeSet<&str> = BTreeSet::new();
+
+        for child in &self.children {
+            if !ids.insert(&child.spec.id) {
+                panic!("Found non-unique child spec id: {:?}!", child.spec.id);
+            }
+        }
+    }
+
+    fn find_child(&mut self, pid: Pid) -> Option<usize> {
+        self.children
+            .iter()
+            .position(|child| child.pid.is_some_and(|cpid| cpid == pid))
+    }
+}
+
+impl SupervisedChild {
+    /// Returns `true` if the child is a temporary process.
+    pub const fn is_temporary(&self) -> bool {
+        matches!(self.spec.restart, Restart::Temporary)
+    }
+
+    /// Returns how the child should be terminated.
+    pub const fn shutdown(&self) -> Shutdown {
+        match self.spec.shutdown {
+            None => match self.spec.child_type {
+                ChildType::Worker => Shutdown::Duration(Duration::from_secs(5)),
+                ChildType::Supervisor => Shutdown::Infinity,
+            },
+            Some(shutdown) => shutdown,
+        }
     }
 }
 
@@ -152,7 +243,129 @@ impl GenServer for Supervisor {
     async fn init(&mut self, _: Self::InitArg) -> Result<(), ExitReason> {
         Process::set_flags(ProcessFlags::TRAP_EXIT);
 
-        // A static supervisor just needs to initialize the children.
         self.init_children().await
     }
+
+    async fn handle_info(&mut self, info: Message<Self::Message>) -> Result<(), ExitReason> {
+        match info {
+            Message::System(SystemMessage::Exit(pid, reason)) => {
+                self.restart_child(pid, reason).await
+            }
+            _ => Ok(()),
+        }
+    }
+}
+
+async fn shutdown(pid: Pid, shutdown: Shutdown) -> Result<(), ExitReason> {
+    let monitor = Process::monitor(pid);
+
+    match shutdown {
+        Shutdown::BrutalKill => shutdown_brutal_kill(pid, monitor).await,
+        Shutdown::Duration(timeout) => shutdown_timeout(pid, monitor, timeout).await,
+        Shutdown::Infinity => shutdown_infinity(pid, monitor).await,
+    }
+}
+
+async fn shutdown_brutal_kill(pid: Pid, monitor: Reference) -> Result<(), ExitReason> {
+    Process::exit(pid, ExitReason::Kill);
+
+    let result = Process::receiver()
+        .ignore_type()
+        .select::<(), _>(|message| {
+            match message {
+                Message::System(SystemMessage::ProcessDown(_, tag, _)) => {
+                    // Make sure that the tag matches.
+                    *tag == monitor
+                }
+                _ => false,
+            }
+        })
+        .await;
+
+    let Message::System(SystemMessage::ProcessDown(_, _, reason)) = result else {
+        unreachable!()
+    };
+
+    unlink_flush(pid, reason);
+
+    Ok(())
+}
+
+async fn shutdown_timeout(
+    pid: Pid,
+    monitor: Reference,
+    timeout: Duration,
+) -> Result<(), ExitReason> {
+    Process::exit(pid, ExitReason::from("shutdown"));
+
+    let receiver = Process::receiver()
+        .ignore_type()
+        .select::<(), _>(|message| {
+            match message {
+                Message::System(SystemMessage::ProcessDown(_, tag, _)) => {
+                    // Make sure that the tag matches.
+                    *tag == monitor
+                }
+                _ => false,
+            }
+        });
+
+    let result = Process::timeout(timeout, receiver).await;
+
+    match result {
+        Ok(Message::System(SystemMessage::ProcessDown(_, _, reason))) => {
+            unlink_flush(pid, reason);
+
+            Ok(())
+        }
+        Ok(_) => unreachable!(),
+        Err(_) => shutdown_brutal_kill(pid, monitor).await,
+    }
+}
+
+async fn shutdown_infinity(pid: Pid, monitor: Reference) -> Result<(), ExitReason> {
+    Process::exit(pid, ExitReason::from("shutdown"));
+
+    let result = Process::receiver()
+        .ignore_type()
+        .select::<(), _>(|message| {
+            match message {
+                Message::System(SystemMessage::ProcessDown(_, tag, _)) => {
+                    // Make sure that the tag matches.
+                    *tag == monitor
+                }
+                _ => false,
+            }
+        })
+        .await;
+
+    let Message::System(SystemMessage::ProcessDown(_, _, reason)) = result else {
+        unreachable!()
+    };
+
+    unlink_flush(pid, reason);
+
+    Ok(())
+}
+
+fn unlink_flush(pid: Pid, default_reason: ExitReason) -> ExitReason {
+    Process::unlink(pid);
+
+    let mut reason = default_reason;
+
+    Process::receiver()
+        .ignore_type()
+        .drop::<(), _>(|message| match message {
+            Message::System(SystemMessage::Exit(epid, ereason)) => {
+                if *epid == pid {
+                    reason = ereason.clone();
+                    return true;
+                }
+
+                false
+            }
+            _ => false,
+        });
+
+    reason
 }
