fe36f1b646f5a5b92a3060f385ac1f9de76d5e38
dtzxporter <dtzxporter@users.noreply.github.com>
2024-05-08 15:35:04 -0400

    Tracing is on by default, no need to customize the macro.

--- hydra-macros/src/entry.rs
+++ hydra-macros/src/entry.rs
@@ -12,21 +12,11 @@ pub(crate) fn main(_: TokenStream, item: TokenStream) -> TokenStream {
     let input_block = &input_fn.block;
     let input_struct = format_ident!("{}_MainStruct", input_name);
 
-    let tracing = cfg!(feature = "tracing");
-    let tracing = tracing.then(|| {
-        quote! {
-            const TRACING_SUBSCRIBE: bool = true;
-            const TRACING_PANICS: bool = true;
-        }
-    });
-
     let output = quote! {
         #[allow(non_camel_case_types)]
         struct #input_struct;
 
         impl ::hydra::Application for #input_struct {
-            #tracing
-
             async fn start(&self) -> Result<::hydra::Pid, ::hydra::ExitReason> {
                 Ok(Process::spawn_link(async move {
                     #input_block
