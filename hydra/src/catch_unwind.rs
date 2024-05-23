068a512eb185304b484e68e7972b3ad1543e5b37
dtzxporter <dtzxporter@users.noreply.github.com>
2024-05-22 23:25:00 -0400

    Remove unused code in catch_unwind.

--- hydra/src/catch_unwind.rs
+++ hydra/src/catch_unwind.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use std::future::Future;
 use std::panic::catch_unwind;
 use std::panic::AssertUnwindSafe;
@@ -9,40 +7,6 @@ use std::task::Context;
 use std::task::Poll;
 
 use pin_project_lite::pin_project;
-
-/// A function that will catch panics and unwind them.
-pub struct CatchUnwind<Fun, R>
-where
-    Fun: FnOnce() -> R + Send + UnwindSafe + 'static,
-    R: Send + 'static,
-{
-    function: Fun,
-}
-
-impl<Fun, R> CatchUnwind<Fun, R>
-where
-    Fun: FnOnce() -> R + Send + UnwindSafe + 'static,
-    R: Send + 'static,
-{
-    /// Constructs a new instance of [CatchUnwind].
-    #[allow(clippy::new_ret_no_self)]
-    pub fn new(function: Fun) -> Result<R, String> {
-        Self { function }.catch_unwind()
-    }
-
-    /// Executes the function returning any error that occured.
-    fn catch_unwind(self) -> Result<R, String> {
-        catch_unwind(self.function).map_err(|x| {
-            if x.is::<String>() {
-                return *x.downcast::<String>().unwrap();
-            } else if x.is::<&str>() {
-                return x.downcast::<&str>().unwrap().to_string();
-            }
-
-            "Unknown error!".to_string()
-        })
-    }
-}
 
 pin_project! {
     /// A future that will catch panics and unwind them.
