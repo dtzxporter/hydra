75756baaf6dbfb54eaab457edb84a0639bbdda16
dtzxporter <dtzxporter@users.noreply.github.com>
2024-06-10 15:04:31 -0400

    Make runtime info public.

--- hydra/src/runtime_info.rs
+++ hydra/src/runtime_info.rs
@@ -9,13 +9,13 @@ use crate::process_len;
 #[derive(Serialize, Deserialize)]
 pub struct RuntimeInfo {
     /// The crate version of the hydra runtime.
-    system_version: String,
+    pub system_version: String,
     /// The number of logical cpus on the current machine.
-    logical_cpus: usize,
+    pub logical_cpus: usize,
     /// The number of worker threads currently in use.
-    worker_threads: usize,
+    pub worker_threads: usize,
     /// The number of processes currently running.
-    processes: usize,
+    pub processes: usize,
 }
 
 impl RuntimeInfo {
